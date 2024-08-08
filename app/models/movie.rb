2bb295a47a306c6510405c2ecb5dce631345a332
ritorocks <ritorocks@gmail.com>
2024-08-08 17:43:40 +0000

    addded guest model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,13 +16,15 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
-  def director
-    key = self.director_id
+  belongs_to(:director)
+  has_many(:actors, through: :characters, source: :actor)
+ # def director
+  #  key = self.director_id
 
-    matching_set = Director.where({ :id => key })
+   # matching_set = Director.where({ :id => key })
 
-    the_one = matching_set.at(0)
+   # the_one = matching_set.at(0)
 
-    return the_one
-  end
+   # return the_one
+   # end
 end
